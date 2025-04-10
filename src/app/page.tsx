b563e46e97bca9a3966ce37fb7381fb11730f351
24LP9559 24LP9559 <24LP9559@G301-CC-206.local>
2025-04-10 12:23:08 +0800

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
+"use client";
 import {
   Movie,
   PopularMovie,
   TopRatedMovie,
   UpcomingMovie,
 } from "@/components";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [user, setUser] = useState();
+  const fetchUsers = async () => {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_ENDPOINT}/user`
+    );
+    console.log(response);
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  console.log(process.env.NEXT_PUBLIC_BACKEND_ENDPOINT);
+
   return (
-    <div className="w-full">
-      <Movie></Movie>
-      <div className="flex flex-col gap-[52px]">
-        <UpcomingMovie />
-        <PopularMovie />
-        <TopRatedMovie />
-      </div>
-    </div>
+    // <div className="w-full">
+    //   <Movie></Movie>
+    //   <div className="flex flex-col gap-[52px]">
+    //     <UpcomingMovie />
+    //     <PopularMovie />
+    //     <TopRatedMovie />
+    //   </div>
+    // </div>
+    <div>hello</div>
   );
 }
